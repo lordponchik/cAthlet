62bf30d47cbe59bfae48e975a9558352490605aa
Denys Strashniuk <d.strashniuk@gmail.com>
2025-11-26 13:50:34 +0100

    verbesern: stylen für layout

--- cAthlet/wwwroot/js/site.js
+++ cAthlet/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+    const logo = document.getElementById('logo');
+
+    function startAnimation() {
+
+       setTimeout(() => {
+        logo.classList.add('animate');
+                }, 10);
+   }
+    startAnimation();
+});
+</script>
